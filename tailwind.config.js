afab63c9e1c746203c6ebedad19f3607d61cfa58
lionger99 <raymond.raynet@gmail.com>
2024-05-05 14:56:34 +0000

    Portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // content: ['./src/**/*.{html,js}'],
+  content: ['index.html'],
+  theme: {
+    container: {
+    center: true,
+    padding: '16px' 
+    },
+    extend: {
+      colors: {
+        primary: '#3b82f6',
+        secondary: '#64748b',
+        dark: '#1e3a8a',
+      },
+      screen: {
+        '2xl': '1320pxl',
+      },
+    },
+  },
+  plugins: [],
+}
+
